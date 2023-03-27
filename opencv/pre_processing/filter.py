c235736cea961df46ba826c1cea83f88eb999fb3
Linus Thorsell <thorsell.linus@gmail.com>
2023-03-27 10:21:35 +0200

    initial

--- opencv/pre_processing/filter.py
+++ opencv/pre_processing/filter.py
@@ -1,12 +1,13 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("./src/cv_input.png")
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+def filter_image(input_url, output_url):
+    img = cv.imread(input_url)
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-lower = np.array([0,113,113])
-higher = np.array([179, 255, 255])
+    lower = np.array([0,113,113])
+    higher = np.array([179, 255, 255])
 
-bin_img_data = cv.inRange(hsv, lower, higher)
+    bin_img_data = cv.inRange(hsv, lower, higher)
 
-cv.imwrite("./src/cv_output.png", bin_img_data)
+    cv.imwrite(output_url, bin_img_data)
