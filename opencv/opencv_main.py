c0f679171a194ab9c1fb52bda9df8a6a44639b68
LinusThorsell <thorsell.linus@gmail.com>
2023-03-28 10:34:17 +0100

    fixed camera capture

--- opencv/opencv_main.py
+++ opencv/opencv_main.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 import time
 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+from picamera2 import Picamera2 as PiCamera
 
 from pre_processing.filter import filter_image
 from pixel_observer.pixel import make_pixel
@@ -11,14 +10,13 @@ from pixel_observer.pixel import make_pixel
 # camera_stream = cv.VideoCapture(0)
 
 camera = PiCamera()
-raw_capture = PiRGBArray(camera)
-time.sleep(0.1)
+camera.start()
+time.sleep(1)
 
 pixel = make_pixel(0.4, 50)
 
 while True:
-    camera.capture(raw_capture, format="bgr")
-    image = raw_capture.array
+    image = camera.capture_array("main")
     #image = cv.imread("./pre_processing/src/cv_input.png")
 
     binary_image = filter_image(image)
