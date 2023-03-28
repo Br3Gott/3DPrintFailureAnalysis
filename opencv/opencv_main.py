d41e289cdd2b1259de10ba1bd23e84a8e5fbb125
LinusThorsell <thorsell.linus@gmail.com>
2023-03-28 07:32:18 +0100

    camera fixes

--- opencv/opencv_main.py
+++ opencv/opencv_main.py
@@ -1,16 +1,25 @@
 import cv2 as cv
 import time
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
 from pre_processing.filter import filter_image
 from pixel_observer.pixel import make_pixel
 
 # Open videostream
-camera_stream = cv.VideoCapture(0)
+# camera_stream = cv.VideoCapture(0)
+
+camera = PiCamera()
+raw_capture = PiRGBArray(camera)
+time.sleep(0.1)
+
 pixel = make_pixel(0.4, 50)
 
 while True:
-    #image = camera_stream.read()
-    image = cv.imread("./pre_processing/src/cv_input.png")
+    camera.capture(raw_capture, format="bgr")
+    image = raw_capture.array
+    #image = cv.imread("./pre_processing/src/cv_input.png")
 
     binary_image = filter_image(image)
 
