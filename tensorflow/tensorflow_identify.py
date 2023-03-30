d2762a33802753ff3fcc06e845f37942b6dd470c
LinusThorsell <thorsell.linus@gmail.com>
2023-03-30 10:08:47 +0100

    Fully integrated print_observer complete

--- tensorflow/tensorflow_identify.py
+++ tensorflow/tensorflow_identify.py
@@ -3,30 +3,45 @@ import numpy as np
 
 import tflite_runtime.interpreter as tflite
 
-# Load the TFLite model and allocate tensors.
-interpreter = tflite.Interpreter(model_path="model.tflite")
-interpreter.allocate_tensors()
+class Indentify:
+    def run(input_image, verbose=False):
 
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+        # Load the TFLite model and allocate tensors.
+        interpreter = tflite.Interpreter(model_path="./tensorflow/model.tflite")
+        class_names = ["Fail", "Success"]
+        interpreter.allocate_tensors()
 
-# Load input image and set input tensor
-img = cv.imread("test_input2.jpg")
-new_img = cv.resize(img, (180, 180))
-new_img = new_img.astype(np.float32)
-new_img = np.expand_dims(new_img, axis=0)
-interpreter.set_tensor(input_details[0]['index'], new_img)
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+        if verbose:
+            print("input details")
+            print(input_details)
+            print("output details")
+            print(output_details)
 
-# Invoke tflite interpreter
-interpreter.invoke()
+        # Load input image and set input tensor
+        # img = cv.imread("./tensorflow/test_input.jpg")
+        img = input_image
+        new_img = cv.resize(img, (180, 180))
+        new_img = new_img.astype(np.float32)
+        # temp fix for binary image
+        new_img = cv.cvtColor(new_img, cv.COLOR_GRAY2RGB)
+        new_img = np.expand_dims(new_img, axis=0)
 
-# Fetch output data from tensor
-output_data = interpreter.get_tensor(output_details[0]['index'])
+        interpreter.set_tensor(input_details[0]['index'], new_img)
 
-# Pretty print result
-class_names = ["Fail", "Success"]
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(output_data)], 100 * np.max(output_data))
-)
+        # Invoke tflite interpreter
+        interpreter.invoke()
+
+        # Fetch output data from tensor
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+
+        # Pretty print result
+        if verbose:
+            print(
+                "This image most likely belongs to {} with a {:.2f} percent confidence."
+                .format(class_names[np.argmax(output_data)], 100 * np.max(output_data))
+            )
+
+        return class_names[np.argmax(output_data)]
