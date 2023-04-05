bf5617744705d9645f53d0063633cd8547d18aaa
thorsell.linus@gmail.com <thorsell.linus@gmail.com>
2023-04-05 10:08:52 +0100

    Reimplemented identification and renamed file/class

--- tensorflow/tensorflow_identify.py
+++ tensorflow/tensorflow_identify.py
@@ -3,46 +3,44 @@ import numpy as np
 
 import tflite_runtime.interpreter as tflite
 
-class Indentify:
+class Identify:
     def run(input_image, verbose=False):
 
-        # Load the TFLite model and allocate tensors.
-        interpreter = tflite.Interpreter(model_path="./tensorflow/model.tflite")
-        class_names = ["Fail", "Success"]
-        interpreter.allocate_tensors()
+        TF_MODEL_FILE_PATH = '/home/pi/exjobb/tensorflow/model.tflite' # The default path to the saved TensorFlow Lite model
 
-        # Get input and output tensors.
-        input_details = interpreter.get_input_details()
-        output_details = interpreter.get_output_details()
-        if verbose:
-            print("input details")
-            print(input_details)
-            print("output details")
-            print(output_details)
+        interpreter = tflite.Interpreter(model_path=TF_MODEL_FILE_PATH)
 
-        # Load input image and set input tensor
-        # img = cv.imread("./tensorflow/test_input.jpg")
-        img = input_image
-        new_img = cv.resize(img, (180, 180))
+        interpreter.get_signature_list()
+
+        classify_lite = interpreter.get_signature_runner('serving_default')
+        classify_lite
+
+        new_img = cv.resize(input_image, (180, 180))
         new_img = new_img.astype(np.float32)
-        # temp fix for binary image
-        #new_img = cv.cvtColor(new_img, cv.COLOR_GRAY2RGB)
         new_img = np.expand_dims(new_img, axis=0)
 
-        interpreter.set_tensor(input_details[0]['index'], new_img)
+        predictions_lite = classify_lite(sequential_input=new_img)["dense_1"]
 
-        # Invoke tflite interpreter
-        interpreter.invoke()
+        # calculate the softmax of a vector
+        def softmax(vector):
+            e = np.exp(vector)
+            return e / e.sum()
 
-        # Fetch output data from tensor
-        output_data = interpreter.get_tensor(output_details[0]['index'])
+        score_lite = softmax(predictions_lite)
 
-        # Pretty print result
-        if verbose:
+        class_names = ["Fail", "Success"]
+
+
+        if (verbose == True):
             print(
-                "This image most likely belongs to {} with a {:.2f} percent confidence."
-                .format(class_names[np.argmax(output_data)], 100 * np.max(output_data))
+                "Is {} with a {:.2f} percent confidence."
+                .format(class_names[0], 100 * score_lite[0][0])
             )
-            print(output_data)
 
-        return class_names[np.argmax(output_data)]
+            print(
+                "Is {} with a {:.2f} percent confidence."
+                .format(class_names[1], 100 * score_lite[0][1])
+            )
+
+
+        return [score_lite[0][0]*100, score_lite[0][1]*100]
