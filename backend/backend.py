dae488c98f8a02b146fcb6e60d717b1c7c97355d
Br3Gott <dawesohl@gmail.com>
2023-04-12 13:47:20 +0100

    Working websockets with frontend and some endpoint data added

--- backend/backend.py
+++ backend/backend.py
@@ -1,3 +1,6 @@
+import subprocess
+import json
+
 import time
 import asyncio
 from aiohttp import web
@@ -16,9 +19,9 @@ async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    for _ws in connections:
-        await _ws.send_str('User joined!')
-        print("User joined!")
+    # for _ws in connections:
+        # await _ws.send_str('User joined!')
+    print("User joined!")
     connections.append(ws)
 
     async for msg in ws:
@@ -27,24 +30,57 @@ async def websocket_handler(request):
                 await ws.close()
             else:
                 print(msg.data)
-                await ws.send_str('Hello client :D')
+                # await ws.send_str('Hello client :D')
         elif msg.type == web.WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
                   ws.exception())
     
     connections.remove(ws)
-    for _ws in connections:
-        await _ws.send_str('User disconected!')
+    # for _ws in connections:
+        # await _ws.send_str('User disconected!')
     print('websocket connection closed')
 
     return ws
 
-async def sendMessage(msg):
-    while True:
-        time.sleep(1)
-        print("Running...")
-        for _ws in connections:
-            await _ws.send_str(msg)
+async def sendMessage(title, msg):
+    print("Sending msg...")
+    for _ws in connections:
+        await _ws.send_str("{\"" + title + "\": " + json.dumps(msg) + "}")
+
+async def getPs():
+    p = subprocess.Popen(["ps", "aux" , "--sort=-pcpu"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(['head', '-10'], stdin=p.stdout,
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = p2.stdout.read()
+    resarr = res.decode("utf-8").split("\n")
+    output = []
+    resarr = resarr[1:]
+    for line in resarr:
+        inner = line.split(" ")
+        clean_inner = []
+        for elem in inner:
+            if (len(elem) > 0):
+                clean_inner.append(elem)
+        output.append(clean_inner)
+    prepared_output = []
+    for line in output:
+        if (len(line) > 0):
+            temp = {}
+            #user
+            temp["user"] = line[0]
+            #process id
+            temp["processId"] = line[1]
+            #cpu
+            temp["cpu"] = line[2]
+            #mem
+            temp["memory"] = line[3]
+            #start
+            temp["start"] = line[8]
+            #command + args
+            command = line[10:]
+            temp["command"] = " ".join(command)
+            prepared_output.append(temp)
+    return prepared_output
 
 app = web.Application()
 app.add_routes(routes)
@@ -56,8 +92,11 @@ async def wrapper():
     await site.start()
 
     while True:
-        await asyncio.sleep(10)
-        await sendMessage("Hello guys :/")
+        # await asyncio.sleep(5)
+        # await sendMessage("regular", "Hello guys :/")
+        await asyncio.sleep(5)
+        ps = await getPs()
+        await sendMessage("ps", ps)
 
 asyncio.run(wrapper())
 
