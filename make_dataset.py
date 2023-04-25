22f5e6a7c403e473c6d390b58a6628bae772eab2
Br3Gott <dawesohl@gmail.com>
2023-04-25 14:54:41 +0200

    Fixed indentation in make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -42,7 +42,7 @@ if len(sys.argv) > 2 and sys.argv[2] == "filter":
                 image_path = ("./datasets/{}/{}").format(folder_name, folder)
                 if (os.path.isfile(os.path.join(image_path, image_name))):
                     image_raw = cv.imread(os.path.join(image_path, image_name))
-                    image_filtered = filter_image(image_raw)
+                    image_filtered, image_masked = filter_image(image_raw)
                     sys.stdout.write("\033[K")
                     current_image += 1
                     print("Writing files {}% [{}/{}]: ({}filtered/{})".format(round((current_image/(total_image_count*2)) * 100), current_image, total_image_count*2, pre_path, image_name), end="\r")
@@ -50,29 +50,29 @@ if len(sys.argv) > 2 and sys.argv[2] == "filter":
                     sys.stdout.write("\033[K")
                     
 
-    os.mkdir("./datasets/{}".format(output_file))
-    os.mkdir("./datasets/{}/fail".format(output_file))
-    os.mkdir("./datasets/{}/success".format(output_file))
+os.mkdir("./datasets/{}".format(output_file))
+os.mkdir("./datasets/{}/fail".format(output_file))
+os.mkdir("./datasets/{}/success".format(output_file))
 
-    print(dataset_folders)
-    for dataset in dataset_folders:
-        print("Adding dataset: ./datasets/{}".format(dataset))
+print(dataset_folders)
+for dataset in dataset_folders:
+    print("Adding dataset: ./datasets/{}".format(dataset))
 
-        folders = [f for f in os.listdir("./datasets/{}".format(dataset)) if "filtered" in f] # keep folders with filtered in their names
+    folders = [f for f in os.listdir("./datasets/{}".format(dataset)) if "filtered" in f] # keep folders with filtered in their names
 
-        for folder in folders:
-            print("Processing folder: ./datasets/{}/{}".format(dataset, folder))
-            
-            pre_path = "./datasets/{}/success/".format(output_file)
-            if "fail" in folder:
-                pre_path = "./datasets/{}/fail/".format(output_file)
+    for folder in folders:
+        print("Processing folder: ./datasets/{}/{}".format(dataset, folder))
+        
+        pre_path = "./datasets/{}/success/".format(output_file)
+        if "fail" in folder:
+            pre_path = "./datasets/{}/fail/".format(output_file)
 
-            for image_name in os.listdir("./datasets/{}/{}".format(dataset, folder)):
-                image_path = ("./datasets/{}/{}").format(dataset, folder)
-                if (os.path.isfile(os.path.join(image_path, image_name))):
-                    shutil.copy(os.path.join(image_path, image_name), os.path.join(pre_path, image_name))
+        for image_name in os.listdir("./datasets/{}/{}".format(dataset, folder)):
+            image_path = ("./datasets/{}/{}").format(dataset, folder)
+            if (os.path.isfile(os.path.join(image_path, image_name))):
+                shutil.copy(os.path.join(image_path, image_name), os.path.join(pre_path, image_name))
 
-    print("Finished copying images")
+print("Finished copying images")
 
 print("Compressing image folder")
 with tarfile.open("./datasets/{}.tar.gz".format(output_file), "w:gz") as tar:
