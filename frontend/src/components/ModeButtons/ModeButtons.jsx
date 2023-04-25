2883a2a8c05a8c0cbc21fd6199e7ad72a8dcf8da
Br3Gott <dawesohl@gmail.com>
2023-04-25 08:21:39 +0200

    Implement failure tracking in frontend

--- frontend/src/components/ModeButtons/ModeButtons.jsx
+++ frontend/src/components/ModeButtons/ModeButtons.jsx
@@ -9,9 +9,25 @@ const Btn = styled.button`
   color: black;
   border: 1px solid black;
   background-color: ${props => props.active ? "lightgreen" : "white"};
+`;
+
+const ControlPanelButtons = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    background-color: white;
+    color: black;
 `
 
-export default function Livestream({ socketUrl }) {
+export default function ModeButtons({ socketUrl }) {
+    
+    const [allowedfailsnum, setallowedfailsnum] = useState(3);
+    const [historylengthnum, sethistorylengthnum] = useState(5);
+
+    const [fails, setfails] = useState(0);
+    const [historylen, sethistorylen] = useState(0);
+
+
     const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, { retryOnError: true });
     const [active, setActive] = useState(false);
 
@@ -22,6 +38,13 @@ export default function Livestream({ socketUrl }) {
             if (res["on"] != null) {
                 setActive(res["on"])
             }
+            else if (res["controlpanel"] != null) {
+                setallowedfailsnum(res["controlpanel"].allowedfails);
+                sethistorylengthnum(res["controlpanel"].historylength);
+
+                setfails(res["controlpanel"].currfails);
+                sethistorylen(res["controlpanel"].currhistorylen);
+            }
         }
     }, [lastMessage]);
 
@@ -29,8 +52,26 @@ export default function Livestream({ socketUrl }) {
         <ModuleContainer>
             <StatusCard>Control Panel</StatusCard>
             <StatusCard>
-                <Btn onClick={() => { sendMessage("activate"); setActive(true) }} active={active}>Activate</Btn>
-                <Btn onClick={() => { sendMessage("deactivate"); setActive(false) }} active={!active}>Deactivate</Btn>
+                <ControlPanelButtons>
+                    <p>Printer protection:</p>
+                </ControlPanelButtons>
+                <ControlPanelButtons>
+                    <Btn onClick={() => { sendMessage("activate"); setActive(true) }} active={active}>Activated</Btn>
+                    <Btn onClick={() => { sendMessage("deactivate"); setActive(false) }} active={!active}>Deactivated</Btn>
+                </ControlPanelButtons>
+                <ControlPanelButtons>
+                    <p>{fails} failed of {historylen}</p>
+                </ControlPanelButtons>
+                <ControlPanelButtons>
+                    <label htmlFor="allowedfails">
+                        <p>Allowed fails</p>
+                        <input id="allowedfails" type="number" value={allowedfailsnum} onChange={ (e) => {sendMessage("allowedfails=" + e.target.value); setallowedfailsnum(e.target.value)} }></input>
+                    </label>
+                    <label htmlFor="historylength">
+                        <p>History length</p>
+                        <input id="historylength" type="number" value={historylengthnum} onChange={ (e) => {sendMessage("historylength=" + e.target.value); sethistorylengthnum(e.target.value)} }></input>
+                    </label>
+                </ControlPanelButtons>
             </StatusCard>
         </ModuleContainer>
     );
